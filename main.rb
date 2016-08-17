154cb712dbe9d00f76c9698060538bac8f839589
AvegaTM <AvegaTemp@GMail.com>
2016-08-17 17:32:33 +0300

    Final v.1 new extended

--- main.rb
+++ main.rb
@@ -1,15 +1,11 @@
 require "readline"
 require "./Password_Generator"
 require "./Say_Password"
-
 hello = 
   "Данная скрипт расчитан под оперционную систему Linux.\n" \
-  "Для его работы необходима программа mplayer.\n" \
-  "Введите длину пароля цифрами > "
-passwd_lng = Readline.readline(hello, true).to_i
-#passwd_lng = 11
-passwd_gen = Password_Generator.new(passwd_lng)
-passwd_gen.generating_password
+  "Для его работы необходима программа mplayer.\n"
+puts hello
+passwd_gen = Password_Generator.new()
 passwd_gen.print_password
 say = Say_Password.new(passwd_gen.get_password)
 say.say_password
