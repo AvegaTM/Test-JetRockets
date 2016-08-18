96c1a15d21b0c5d490d9472fed11dc56341a349e
AvegaTM <AvegaTemp@GMail.com>
2016-08-18 07:09:35 +0300

    I do not know the final or not.

--- Password_Generator.rb
+++ Password_Generator.rb
@@ -1,8 +1,16 @@
 class Password_Generator
            
-      def initialize
+      def initialize(password_length)
+          @password = ''
+          if password_length < 1
+            puts "Динна пароля должна быть положительным числом больше 0."
+            return ''
+          end 
+          work_array = []
           file_array = File.new("dict.txt", "r").to_a
-          @password = file_array[rand(file_array.length)][/^\w+/] 
+          file_array.each{ |line| work_array << line[/^\w+/]}
+          @password += work_array[rand(work_array.length)] while @password.length <= password_length
+          @password = @password.byteslice(1, password_length) if @password.length > password_length
       end
       
       def get_password
